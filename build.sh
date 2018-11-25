95298d4739829793ac4bc6f232b52bcfac947d0b
Mike P. Sinn <m@quantimodo.com>
2018-11-25 14:39:11 -0600

    Paths

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
-SCRIPT_FOLDER=`dirname ${SCRIPT_PATH}` && cd ${SCRIPT_FOLDER} && cd ../.. && export BUILDER_PATH="$PWD" && export IONIC_PATH=${BUILDER_PATH}/ionic
+export BUILDER=`dirname ${SCRIPT_PATH}` && export IONIC=${BUILDER}/ionic
 echo "======================="
 echo "Building Ionic Web App"
 echo "======================="
-cd ${BUILDER_PATH} && CURRENT_GIT_HASH=`git rev-parse @:./ionic` && lastCommitBuilt=`cat ${BUILDER_PATH}/log/ionic-last-commit-built`
+cd ${BUILDER} && CURRENT_GIT_HASH=`git rev-parse @:./ionic` && lastCommitBuilt=`cat ${BUILDER}/log/ionic-last-commit-built`
 echo "Previous IONIC commit built ${lastCommitBuilt} and CURRENT_GIT_HASH ${CURRENT_GIT_HASH}"
 if [[ -z "$lastCommitBuilt" || ${CURRENT_GIT_HASH} != ${lastCommitBuilt} || ${REBUILD} == "1" ]]
     then
-        cd ${IONIC_PATH}
+        cd ${IONIC}
         echo "Removing old combined/minified files.  It should already be done in gulp cleanCombinedFiles but it doesn't seem to be working"
-        rm ${IONIC_PATH}/www/scripts/*
+        rm ${IONIC}/www/scripts/*
         echo "==== RUNNING npm install silently FOR IONIC APP ===="
-        cd ${IONIC_PATH} && npm install --silent
+        cd ${IONIC} && npm install --silent
         echo "==== RUNNING bower install --allow-root --quiet FOR IONIC APP ===="
         bower install --allow-root --quiet
         echo "==== RUNNING gulp configureAppAfterNpmInstall FOR IONIC APP ===="
@@ -21,14 +21,14 @@ if [[ -z "$lastCommitBuilt" || ${CURRENT_GIT_HASH} != ${lastCommitBuilt} || ${RE
             echo "===== IONIC BUILD FAILURE: Ionic success file does not exist so build did not complete! ====="
             exit 1
         fi
-        echo ${CURRENT_GIT_HASH} > ${BUILDER_PATH}/log/ionic-last-commit-built
+        echo ${CURRENT_GIT_HASH} > ${BUILDER}/log/ionic-last-commit-built
     else
         echo "Already built ${CURRENT_GIT_HASH}";
 fi
 echo "======================"
 echo "Building App Designer"
 echo "======================"
-cd ${BUILDER_PATH} && CURRENT_GIT_HASH=$(git rev-parse HEAD) && lastCommitBuilt=`cat ${BUILDER_PATH}/log/qm-docker-last-commit-built`
+cd ${BUILDER} && CURRENT_GIT_HASH=$(git rev-parse HEAD) && lastCommitBuilt=`cat ${BUILDER}/log/qm-docker-last-commit-built`
 echo "Previous QM-DOCKER commit built ${lastCommitBuilt} and CURRENT_GIT_HASH ${CURRENT_GIT_HASH}"
 if [[ -z "$lastCommitBuilt" || ${CURRENT_GIT_HASH} != ${lastCommitBuilt} || ${REBUILD} == "1" ]]
     then
@@ -39,7 +39,7 @@ if [[ -z "$lastCommitBuilt" || ${CURRENT_GIT_HASH} != ${lastCommitBuilt} || ${RE
         gulp default
         test -e success || echo "===== QM-DOCKER BUILD FAILURE: Success file was not deleted so build did not complete! ====="
         test -e success || exit 1
-        echo ${CURRENT_GIT_HASH} > ${BUILDER_PATH}/log/qm-docker-last-commit-built
+        echo ${CURRENT_GIT_HASH} > ${BUILDER}/log/qm-docker-last-commit-built
     else
         echo "Already built ${CURRENT_GIT_HASH}";
 fi
