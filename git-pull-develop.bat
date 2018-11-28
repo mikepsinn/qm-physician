72eb4fced760da5d6e017440f129555302e8598d
Mike P. Sinn <m@quantimodo.com>
2018-11-27 20:13:15 -0600

    ionic update

--- git-pull-develop.bat
+++
@@ -1,6 +0,0 @@
-if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
-echo Pruning tracked branches no longer on remote
-git -c diff.mnemonicprefix=false -c core.quotepath=false fetch --prune origin
-git checkout develop
-git pull
-pause
